b1dfdad989b3e7bb09d072254b19ba8929accc3a
Jorge <jcarapia83@Gmail.com>
2016-12-05 14:58:11 -0800

    testing npm prompt package

--- ticTacToe.js
+++ ticTacToe.js
@@ -1,1 +1,20 @@
-ticTacToe.js
+var prompt = require('prompt');
+
+var board = [
+		['0','1','2'], 
+		['3','4','5'],
+		['6','7','8']
+	];
+
+prompt.start();
+
+var choice;
+
+prompt.get(['choice'], function(err, result){
+	choice = result;
+
+	console.log('you have chosen: ', choice.choice);
+});
+
+//console.log('you have chosen: ', choice);
+
